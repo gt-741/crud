8e3e5f4021729fb4a96eae963b72df5d1dbe229b
guillermo <guillermotorrealba993@gmail.com>
2022-06-08 22:22:07 -0400

    Modificacion del crud
    - Mofidicacion de los atributos de  mayusculas a minusculas
    
    this close #1

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_04_005120) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_09_010337) do
   create_table "estudiantes", force: :cascade do |t|
-    t.string "Nombre"
-    t.string "Apellido"
-    t.integer "Edad"
-    t.string "Cuerso"
-    t.string "Nota"
+    t.string "nombre"
+    t.string "apellido"
+    t.integer "edad"
+    t.string "curso"
+    t.string "nota"
     t.string "varchar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
